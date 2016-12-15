8a3372abe20fcdcab38dad2f35f1a971a0013255
satrong <satrong@163.com>
2016-12-15 22:08:29 +0800

    去掉控制台输出信息减少耗时

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-const AutoAddHash = require("./lib/AutoAddHash");
-let options = require("./config");
-new AutoAddHash(options).start();
+const cp = require("child_process");
+const readline = require('readline');
+const repl = require('repl');
+
+function showDot(txt) {
+    readline.clearLine(process.stdout, 0);
+    readline.cursorTo(process.stdout, 0, null);
+    process.stdout.write(txt);
+}
+let len = 0, t = Date.now();
+let f = setInterval(() => {
+    showDot(`执行中${'.'.repeat(len++)}`);
+    len = len === 4 ? 0 : len;
+}, 500);
+
+let child = cp.fork(`${__dirname}/lib/AutoAddHash.js`);
+
+child.on("message", data => {
+    clearInterval(f);
+    repl.start({
+        prompt: `已修改${data.success}处，未变化${data.noChange}处，错误日志请查看error.log文件，耗时${((Date.now()-t)/1000).toFixed(2)-0}s\n按Enter键可退出`, eval: function () {
+            process.exit();
+        }
+    });
+});
